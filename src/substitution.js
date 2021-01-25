6ebaac3ff527a748ef12bf1947a2e9c6b4f57325
Hunter Marshall <beidah@gmail.com>
2021-01-25 13:06:16 -0600

    Substitution cypher

--- src/substitution.js
+++ src/substitution.js
@@ -5,9 +5,64 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
+  const plaintext_alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  function invalidSubstitutionAlphabet(alphabet) {
+    if (alphabet.length != 26) {
+      return true;
+    }
+
+    const chars = alphabet.split("");
+    return chars.some(function(v,i,a){
+      return a.lastIndexOf(v)!=i;
+    });
+  }
+
+  function substitution_encode(input, alphabet) {
+    let cyphertext = "";
+
+    for (let i = 0; i < input.length; i++) {
+      const char = input[i];
+      const charIdx = plaintext_alphabet.indexOf(char);
+      if (charIdx != -1) {
+        cyphertext += alphabet[charIdx];
+      } else {
+        cyphertext += char;
+      }
+    }
+
+    return cyphertext;
+  }
+
+  function substitution_decode(input, alphabet) {
+    let plaintext = "";
+
+    for (let i = 0; i < input.length; i++) {
+      const char = input[i];
+      const charIdx = alphabet.indexOf(char);
+      if (charIdx != 1) {
+        plaintext += plaintext_alphabet[charIdx];
+      } else {
+        plaintext += char;
+      }
+    }
+
+    return plaintext;
+  }
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+    input = input.toLowerCase();
+
+    if (invalidSubstitutionAlphabet(alphabet)) {
+      return false;
+    }
+
+    if (encode) {
+      return substitution_encode(input, alphabet);
+    } else {
+      return substitution_decode(input, alphabet);
+    }
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,47 @@
 // Write your tests here!
+const expect = require("chai").expect;
+const substitution = require("../src/substitution");
+
+describe("Substitution cypher", () => {
+    it("should return false if the alphabet isn't exactly 26 characters long", () => {
+        let actual = substitution("message", "short");
+        expect(actual).to.be.false;
+        actual = substitution("message", "longstringlongstringlongstringlongstring");
+        expect(actual).to.be.false;
+    });
+
+    it("should return false if the alphabet contains repeated characters", () => {
+        let actual = substitution("thinkful", "abcabcabcabcabcabcabcabcyz");
+        expect(actual).to.be.false;
+    });
+
+    it("should correct encrypt text", () => {
+        let actual = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev"); 
+        let expected = 'jrufscpw';
+        expect(actual).to.equal(expected);
+    });
+
+    it("should correctly decrypt text", () => {
+        let actual = substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false); 
+        let expected = 'thinkful';
+        expect(actual).to.equal(expected);
+    });
+
+    it("should preserve spaces in encryption", () => {
+        let actual = substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev"); 
+        let expected = 'elp xhm xf mbymwwmfj dne';
+        expect(actual).to.equal(expected);
+    });
+
+    it("should properly encrypt text when there are special characters in the substitution alphabet", () => {
+        let actual = substitution("message", "$wae&zrdxtfcygvuhbijnokmpl"); 
+        let expected = "y&ii$r&";
+        expect(actual).to.equal(expected);
+    });
+
+    it("should properly decrypt text when there are special characters in the substitution alphabet", () => {
+        let actual = substitution("y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false); 
+        let expected = "message";
+        expect(actual).to.equal(expected);
+    })
+})
