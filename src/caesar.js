8420eeec3b74ac52d15d423843df2744bc479608
Hunter Marshall <beidah@gmail.com>
2021-01-25 11:06:29 -0600

    first commit

---
+++ src/caesar.js
@@ -0,0 +1,52 @@
+// Please refrain from tampering with the setup code provided here,
+// as the index.html and test files rely on this setup to work properly.
+// Only add code (e.g., helper methods, variables, etc.) within the scope
+// of the anonymous function on line 6
+
+const caesarModule = (function () {
+  // you can add any code you want within this function scope
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  function caesar_encode(input, shift) {
+    input = input.toLowerCase();
+    let encoded_text = "";
+    
+    for (let i = 0; i < input.length; i++) {
+      const char = input[i];
+      const letter_idx = alphabet.indexOf(char);
+      if (letter_idx != -1) {
+        let cypher_idx = (letter_idx + shift) % 26;
+        encoded_text += alphabet[cypher_idx];
+      } else {
+        encoded_text += input[i];
+      }
+
+      console.log(encoded_text);
+    }
+
+    return encoded_text;
+  }
+
+  function caesar_decode(input, shift) {
+    return caesar_encode(input, -shift);
+  }
+
+  function caesar(input, shift, encode = true) {
+    // your solution code here
+    if (!shift || shift == 0 || shift > 25 || shift < -25) {
+      return false;
+    }
+
+    if (encode) {
+      return caesar_encode(input, shift);
+    } else {
+      return caesar_decode(input, shift);
+    }
+  }
+
+  return {
+    caesar,
+  };
+})();
+
+module.exports = caesarModule.caesar;
---
+++ test/caesar.test.js
@@ -0,0 +1,41 @@
+// Write your tests here!
+const expect = require("chai").expect;
+const caesar = require("../src/caesar");
+
+describe("Caesar cypher", () => {
+    it("should return false if the shift value is equal to 0, less than -25, greater than 25, or not present", () => {
+        expect(caesar("thinkful")).to.be.false;
+        expect(caesar("thinkful", 99)).to.be.false;
+        expect(caesar("thinkful", -26)).to.be.false;
+    });
+
+    it("should return the correct cypher text when given a message and correct shift value", () => {
+        let actual = caesar("thinkful", 3);
+        let expected = "wklqnixo";
+        expect(actual).to.eq(expected);
+    });
+
+    it("should return the correct plaintext when given a message and a correct shift value", () => {
+        let actual = caesar("wklqnixo", 3, false);
+        let expected = "thinkful";
+        expect(actual).to.be.eq(expected);
+    });
+
+    it("should ignore capitals", () => {
+        let actual = caesar("A Message", 4);
+        let expected = caesar("a message", 4);
+        expect(actual).to.be.eq(expected);
+    });
+
+    it("should handle shifts that go past the end of the alphabet", () => {
+        let actual = caesar("z", 3);
+        let expected = "c";
+        expect(actual).to.be.eq(expected);
+    });
+
+    it("should maintain spaces and non-alphabetic characters", () => {
+        let actual = caesar("This is a secret message!", 8);
+        let expected = 'bpqa qa i amkzmb umaaiom!';
+        expect(actual).to.be.eq(expected);
+    })
+})
